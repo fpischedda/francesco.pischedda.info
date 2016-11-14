b5b11ee1696cae6958c8a64a7673a3741078bc94
e_fpischedda <francesco.pischedda@abinsula.com>
2016-11-14 11:43:29 +0100

    new post, new build, added deply command

--- conf.py
+++ conf.py
@@ -564,11 +564,11 @@ REDIRECTIONS = []
 # to `nikola deploy`.  If no arguments are specified, a preset
 # named `default` will be executed.  You can use as many presets
 # in a `nikola deploy` command as you like.
-# DEPLOY_COMMANDS = {
-#     'default': [
-#         "rsync -rav --delete output/ joe@my.site:/srv/www/site",
-#     ]
-# }
+DEPLOY_COMMANDS = {
+    'default': [
+        "rsync -azP output/ blog:sites/francesco.pischedda.info",
+    ]
+}
 
 # github_deploy configuration
 # For more details, read the manual:
